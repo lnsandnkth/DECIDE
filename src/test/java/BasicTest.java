7398aa56768865522b05a0f60e3fa9620627b94d
Maxou <5208681+mxyns@users.noreply.github.com>
2022-01-27 18:58:06 +0100

    test basic unit tests

---
+++ src/test/java/BasicTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BasicTest {
+
+    @Test
+    @DisplayName("A passing test")
+    public void positiveTest() {
+        assert(true);
+    }
+
+    @Test
+    @DisplayName("Catching an Exception")
+    public void negativeTest() {
+        assertThrows(Exception.class, () -> {
+            throw new Exception();
+        });
+    }
+}
