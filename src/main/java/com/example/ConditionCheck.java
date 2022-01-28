c185832ab012d9f8f94520209a734ba9b59693c4
Maxou <5208681+mxyns@users.noreply.github.com>
2022-01-28 19:06:31 +0100

    feat, doc, test - added tests for math functions, documentation and math functions (inside circle check, vector norm comp.)

--- src/main/java/com/example/ConditionCheck.java
+++ src/main/java/com/example/ConditionCheck.java
@@ -1,10 +1,40 @@
 package com.example;
 
-import com.example.Point;
 public class ConditionCheck {
 
+    /**
+     * Computes the distance between two points
+     * @param p1 first Point
+     * @param p2 second Point
+     * @see Point
+     * @return (double) distance between p1 and p2
+     */
+    public static double calDistance(Point p1, Point p2) {
 
-    private double calDistance(Point p1, Point p2) {
         return Math.sqrt(Math.pow(p1.getX() - p2.getX(), 2) + Math.pow(p1.getY() - p2.getY(), 2));
     }
+
+    /**
+     * Computes the length / size / norm of a vector
+     * @param p vector
+     * @see Point
+     * @return (double) length / size / norm of p
+     */
+    public static double calNorm(Point p) {
+
+        return Math.sqrt(Math.pow(p.getX(), 2) + Math.pow(p.getY(), 2));
+    }
+
+    /**
+     * Checks if a point is inside (or on the border) of a circle centered on (0, 0)
+     * @param p point
+     * @param radius radius of the circle
+     * @see Point
+     * @return (boolean) true if Point p is inside (or on the border) of the circle
+     */
+    public static boolean isInCircle(Point p, double radius) {
+
+        return calNorm(p) <= radius;
+    }
+
 }
