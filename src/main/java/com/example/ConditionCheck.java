e8f9c6de21daf47fca13e05bbd7facd8b5e6648f
noahra <noah.rahimzadagan@protonmail.com>
2022-01-29 17:30:16 +0100

    LIC2 implemented along with its corresponding test.

--- src/main/java/com/example/ConditionCheck.java
+++ src/main/java/com/example/ConditionCheck.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import com.example.Point;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -12,10 +10,11 @@ import static java.lang.Math.sqrt;
 public class ConditionCheck {
 
     private double PI = 3.1415926535;
+
     /*
-    * LIC0 checks if there exists at least one set of two consecutive data points
-    *  that are a distance greater than the length
-    * */
+     * LIC0 checks if there exists at least one set of two consecutive data points
+     *  that are a distance greater than the length
+     * */
     public boolean LIC0(List<Point> points, double length1) throws IllegalArgumentException {
         if (length1 < 0) {
             throw new IllegalArgumentException("Length should not be less than zero");
@@ -37,39 +36,42 @@ public class ConditionCheck {
     }
 
 
+    /**
+     * LIC2 checks if there is an angle in the second point of a triangle that consists of three consecutive points that
+     * either is less than PI - epsilon or greater than PI + epsilon.
+     *
+     * @param points  a list of data points
+     * @param epsilon a value for deviation
+     * @return boolean
+     */
+    public boolean LIC2(List<Point> points, float epsilon)  {
+        Queue<Point> pointQueue = new LinkedList<>();
 
-    public boolean LIC2(List<Point> points, double pi, double epsilon) throws IllegalArgumentException {
-        Queue<Point> pointQueue = new LinkedList<Point>();
-        for (int i=0;i<points.size();i++){
-            pointQueue.add(points.get(i));
+        for (Point point : points) {
+            pointQueue.add(point);
             if (pointQueue.size() == 3) {
                 Point a = pointQueue.poll();
                 Point b = pointQueue.poll();
                 Point c = pointQueue.poll();
-                double angleInB = calcAngleInP2(a,b,c);
-                
+                pointQueue.add(b);
+                pointQueue.add(c);
+                double angleInB = calcAngleInPoint2(a, b, c);
+                if (angleInB < (180 - epsilon) || angleInB > (180 + epsilon)) {
+                    return true;
+                }
             }
-
         }
-        return true;
+        return false;
     }
 
-    private double calcAngleInP2(Point p1, Point p2, Point p3) {
-        double a2 = calDistance(p2,p3);
-        double b2 = calDistance(p1,p3);
-        double c2 = calDistance(p1,p2);
-        float a = (float)sqrt(a2);
-        float c = (float)sqrt(c2);
-        float angleInP2 = (float) acos((a2 + c2 - b2)/(2*a*c));
+    private double calcAngleInPoint2(Point p1, Point p2, Point p3) {
+        double a2 = Math.pow(calDistance(p2, p3), 2);
+        double b2 = Math.pow(calDistance(p1, p3), 2);
+        double c2 = Math.pow(calDistance(p1, p2), 2);
+        float a = (float) sqrt(a2);
+        float c = (float) sqrt(c2);
+        float angleInP2 = (float) acos((a2 + c2 - b2) / (2 * a * c));
         angleInP2 = (float) (angleInP2 * 180 / PI);
-        return sqrt(angleInP2);
+        return angleInP2;
     }
-
-
-
-
-
-
-
-
 }
--- src/test/java/ConditionTest.java
+++ src/test/java/ConditionTest.java
@@ -1,5 +1,6 @@
 import com.example.Point;
 import com.example.ConditionCheck;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,4 +21,24 @@ public class ConditionTest {
         boolean res = conditionCheck.LIC0(points, length1);
         assert(res);
     }
+    @Test
+    public void LIC2Test() {
+        List<Point> positiveOutComePointList = List.of(new Point(4, 5), new Point(1, 1), new Point(1,5));
+        float positiveEpsilon = 10;
+        boolean res1 = conditionCheck.LIC2(positiveOutComePointList, positiveEpsilon);
+
+        List<Point> negativeOutComePointList = List.of(new Point(4, 5), new Point(1, 5), new Point(1,1));
+        float negativeEpsilon = 90;
+        boolean res2 = conditionCheck.LIC2(negativeOutComePointList, negativeEpsilon);
+
+        List<Point> secondPositiveOutComePointList = List.of(new Point(4, 5), new Point(1, 5),
+                new Point(1,1),new Point(4, 5), new Point(1, 1), new Point(1,5));
+        boolean res3 = conditionCheck.LIC2(secondPositiveOutComePointList,negativeEpsilon);
+
+        Assertions.assertTrue(res1);
+        Assertions.assertFalse(res2);
+        Assertions.assertTrue(res3);
+    }
+
+
 }
