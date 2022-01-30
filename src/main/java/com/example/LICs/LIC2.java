b3ed1068997c5acf65847a9de3652b9a0527bd65
lnsandn <98472625+lnsandnkth@users.noreply.github.com>
2022-01-30 11:20:53 +0100

    merged main into issue #3
    merged main into issue #3

--- src/main/java/com/example/LICs/LIC2.java
+++ src/main/java/com/example/LICs/LIC2.java
@@ -1,4 +1,36 @@
 package com.example.LICs;
 
+import com.example.utils.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LIC2 {
+    /**
+     * LIC2 checks if there is an angle in the second point of a triangle that consists of three consecutive points that
+     * either is less than PI - epsilon or greater than PI + epsilon.
+     *
+     * @param points  a list of data points
+     * @param epsilon a value for deviation
+     * @return boolean
+     */
+    public static boolean LIC2(List<Point> points, float epsilon) {
+        Queue<Point> pointQueue = new LinkedList<>();
+
+        for (Point point : points) {
+            pointQueue.add(point);
+            if (pointQueue.size() == 3) {
+                Point a = pointQueue.poll();
+                Point b = pointQueue.poll();
+                Point c = pointQueue.poll();
+                pointQueue.add(b);
+                pointQueue.add(c);
+                double angleInB = MathFunctions.calcAngleInPoint2(a, b, c);
+                if (angleInB < (180 - epsilon) || angleInB > (180 + epsilon)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/ConditionTests/LIC2Test.java
+++ src/test/java/ConditionTests/LIC2Test.java
@@ -1,4 +1,32 @@
 package ConditionTests;
 
+import com.example.LICs.*;
+import com.example.utils.Point;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class LIC2Test {
+    @Test
+    @DisplayName("LIC2 Test: ")
+    public void LIC2Test() {
+        List<Point> positiveOutComePointList = Arrays.asList(new Point(4, 5), new Point(1, 1), new Point(1,5));
+        float positiveEpsilon = 10;
+        boolean res1 = LIC2.LIC2(positiveOutComePointList, positiveEpsilon);
+
+        List<Point> negativeOutComePointList = Arrays.asList(new Point(4, 5), new Point(1, 5), new Point(1,1));
+        float negativeEpsilon = 90;
+        boolean res2 = LIC2.LIC2(negativeOutComePointList, negativeEpsilon);
+
+        List<Point> secondPositiveOutComePointList = Arrays.asList(new Point(4, 5), new Point(1, 5),
+                new Point(1,1),new Point(4, 5), new Point(1, 1), new Point(1,5));
+        boolean res3 = LIC2.LIC2(secondPositiveOutComePointList,negativeEpsilon);
+
+        Assertions.assertTrue(res1);
+        Assertions.assertFalse(res2);
+        Assertions.assertTrue(res3);
+    }
 }
