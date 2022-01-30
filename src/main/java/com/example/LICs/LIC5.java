c1b63ebcc18eae6dd860c16c0e98190d0181f856
lnsandn <98472625+lnsandnkth@users.noreply.github.com>
2022-01-30 11:11:30 +0100

    merged main into issue #6
    merged main into issue #6

--- src/main/java/com/example/LICs/LIC5.java
+++ src/main/java/com/example/LICs/LIC5.java
@@ -1,4 +1,21 @@
 package com.example.LICs;
 
+import java.util.List;
+import com.example.utils.*;
+
 public class LIC5 {
+    /**
+     * LIC5 - Launch Interceptor Condition #5
+     * Checks if there is at least one pair of consecutive points in the list with a negative X coordinate difference
+     * @param points list of points to check
+     * @return true if two consecutive points have a negative delta X
+     */
+    public static boolean LIC5(List<Point> points) {
+
+        for (int i = 1; i < points.size(); i++)
+            if (points.get(i).getX() - points.get(i-1).getX() < 0)
+                return true;
+
+        return false;
+    }
 }
