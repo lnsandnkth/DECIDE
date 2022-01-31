e7e92edc89de1de22214b708302c1008b8999b48
Maxou <5208681+mxyns@users.noreply.github.com>
2022-01-31 23:10:25 +0100

    doc - documentation for current state of makeCMV (#30)

--- src/main/java/com/example/ConditionCheck.java
+++ src/main/java/com/example/ConditionCheck.java
@@ -7,6 +7,17 @@ import java.util.List;
 
 public class ConditionCheck {
 
+    /**
+     * Makes a CMV vector by applying the 15 different LICs to the point list with the given parameters
+     * @param points input points of the program
+     * @param LENGTH1 parameter for LIC0
+     * @param RADIUS1 parameter for LIC1
+     * @param EPSILON parameter for LIC2
+     * @param AREA1 parameter for LIC3
+     * @param Q_PTS parameter for LIC4
+     * @param QUADS parameter for LIC5
+     * @return a CMV vector (result of the 15 LICs)
+     */
     public static boolean[] makeCMV(List<Point> points,
                                     double LENGTH1,
                                     double RADIUS1,
