1586a4e9632bc03139deee42d78c97119069d421
xinmiao <>
2022-02-02 13:07:40 +0100

    feat - add global parameters and modify CMV input (#50)

---
+++ src/main/java/com/example/utils/PARAMETERS_T.java
@@ -0,0 +1,27 @@
+package com.example.utils;
+
+public class PARAMETERS_T {
+    /**
+     * inputs to the DECIDE() function
+     */
+    // follows definition on page 10
+    public double LENGTH1;      // Length in LICs 0, 7, 12
+    public double RADIUS1;      // Radius in LICs 1, 8, 13
+    public double EPSILON;      // Deviation from PI in LICs 2, 9
+    public double AREA1;        // Area in LICs 3, 10, 14
+    public int Q_PTS;           // No. of consecutive points in LIC 4
+    public int QUADS;           // No. of quadrants in LIC 4
+    public double DIST;         // Distance in LIC 6
+    public int N_PTS;           // No. of consecutive pts . in LIC 6
+    public int K_PTS;           // No. of int. pts. in LICs 7, 12
+    public int A_PTS;           // No. of int. pts. in LICs 8, 13
+    public int B_PTS;           // No. of int. pts. in LICs 8, 13
+    public int C_PTS;           // No. of int. pts. in LIC 9
+    public int D_PTS;           // No. of int. pts. in LIC 9
+    public int E_PTS;           // No. of int. pts. in LICs 10, 14
+    public int F_PTS;           // No. of int. pts. in LICs 10, 14
+    public int G_PTS;           // No. of int. pts. in LIC 11
+    public double LENGTH2;      // Maximum length in LIC 12
+    public double RADIUS2;      // Maximum radius in LIC 13
+    public double AREA2;        // Maximum area in LIC 14
+}
